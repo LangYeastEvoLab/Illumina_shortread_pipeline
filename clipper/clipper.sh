164d0dbeb2762ea505bd1a986b44add2192bf325
LangYeastEvoLab <LangYeastEvoLab@gmail.com>
2018-11-21 16:19:28 -0500

    updated clipper scripts

--- clipper/clipper.sh
+++ clipper/clipper.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
-# 02/25/28 by KFisher  Kjf214@lehigh.edu
-# shell script to clip/trim FASTQ sequence files 
+
+
+# Script to clip/trim Illumina adapter sequences from demultiplexed fastq files
+# Script written for all 96 barcodes - may need to be modified based on barcode usage 
+
+# Modify upon use with specific files names and locations 
+# UPPERCASE lines indicate code that needs to be specified for each use 
+# all paths should be absolute paths 
 
 for N5 in 02 03 04 05 06 07 08 17
 do
 	for N7 in 01 02 03 04 05 06 07 08 09 10 11 12
 	do
-		# gunzip /share/ceph/gil213group/shared/AutodipSplitFastqs/N5$N5\N7$N7\splitreads-read-1.fastq.gz
-		fastx_clipper -a CTGTCTCTTATACACATCT -i /share/ceph/gil213group/shared/AutodipSplitFastqs/N5$N5\N7$N7\splitreads-read-1.fastq -o /share/ceph/gil213group/shared/N5$N5\N7$N7\splitreads-read-1-clipped.fastq -Q33 -n 18
+		# gunzip PATH/TO/N5$N5\N7$N7\splitreads-read-1.fastq.gz
+		fastx_clipper -a CTGTCTCTTATACACATCT -i PATH/TO/N5$N5\N7$N7\splitreads-read-1.fastq -o PATH/TO/N5$N5\N7$N7\splitreads-read-1-clipped.fastq -Q33 -n 18
 		date
-
 	done	
 done
--- clipper/clipper.slurm.sh
+++ clipper/clipper.slurm.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Modify for each use 
+     # --mail-user = 
+     # Export path to directory containing splitter script
+
 #SBATCH --partition=all-cpu
 #SBATCH --qos=nogpu
 #SBATCH --time=24:00:00
@@ -7,9 +11,9 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --job-name=clipper.sh
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=kjf214@lehigh.edu
+#SBATCH --mail-user=YOU@LEHIGH.EDU
 
 module load fastx-toolkit/0.0.14
-export PATH=$PATH:/share/ceph/gil213group/shared/
+export PATH=$PATH:/PATH/TO/DIRECTORY CONTAINING CLIPPER SCRIPT
 clipper.sh
 exit
