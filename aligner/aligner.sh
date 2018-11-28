1fb0d0bdcbe5f6ca76ea59e5ea480aab4bda18d2
Lang Lab <44009945+LangYeastEvoLab@users.noreply.github.com>
2018-11-28 14:46:45 -0500

    updated aligner script

--- aligner/aligner.sh
+++ aligner/aligner.sh
@@ -7,32 +7,22 @@
 		
 	
 bwa index PATH/TO/reference_files/W303_ann_swb_11-29-17.fasta
-# start by indexing your reference genome file
+samtools faidx PATH/TO/reference_files/W303_ann_swb_11-29-17.fasta
+# start by indexing your reference genome file 
 	
 for N5 in 02 03 04 05 06 07 08 17
 do
 	for N7 in 01 02 03 04 05 06 07 08 09 10 11 12
 	do	
-		 bwa aln -n 0.04 -o 1 -e -1 -d 16 -i 0 -O 5 -E 4 PATH/TO/reference_files/W303_ann_swb_11-29-17.fasta PATH/TO/N5$N5\N7$N7\splitreads-read-1-clipped.fastq > PATH/TO/N5$N5\N7$N7\-clipped.sai	
+	
+	bwa aln -n 0.04 -o 1 -e -1 -d 16 -i 0 -O 5 -E 4 PATH/TO/reference_files/W303_ann_swb_11-29-17.fasta PATH/TO/N5$N5\N7$N7\-clipped.fastq > PATH/TO/N5$N5\N7$N7\.sai	
 
-              bwa samse -n 3 PATH/TO/reference_files/W303_ann_swb_11-29-17.fasta PATH/TO/N5$N5\N7$N7\-clipped.sai PATH/TO/N5$N5\N7$N7\splitreads-read-1-clipped.fastq > PATH/TO/N5$N5\N7$N7\-clipped.sam
+        bwa samse -n 3 PATH/TO/reference_files/W303_ann_swb_11-29-17.fasta PATH/TO/N5$N5\N7$N7\.sai PATH/TO/N5$N5\N7$N7\-clipped.fastq > PATH/TO/N5$N5\N7$N7\.sam
+	
+	samtools view -bS N5$N5\N7$N7\.sam > N5$N5\N7$N7\.bam
 		
-
-
-# the above command uses both paired end files to generate one sam file 
-
-		samtools faidx PATH/TO/reference_files/W303_ann_swb_11-29-17.fasta
+	samtools sort N5$N5\N7$N7\.bam N5$N5\N7$N7\-sorted.bam
 		
-		samtools view -bS N5$N5\N7$N7\.sam > N5$N5\N7$N7\.bam
-		
-# the above command generates a bam file from the sam file 
-		samtools sort N5$N5\N7$N7\.bam N5$N5\N7$N7\-sorted.bam
-		
-# the above command sorts the bam file so that it is viewable
-		samtools index N5$N5\N7$N7\-sorted.bam
-# index your bam file 
-		freebayes -f PATH/TO/reference_files/W303_ann_swb_11-29-17.fasta -p 2 --pooled-discrete N5$N5\N7$N7\-sorted.bam > N5$N5\N7$N7\.vcf
-#n freebayes will call differences between the reference fasta you provide and your bam file. These will be sotred in your vcf. 
-		
+	samtools index N5$N5\N7$N7\-sorted.bam
 	done 
 done 
